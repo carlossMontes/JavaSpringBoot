a85f69dae8f82589e638e3d785f369690c7fa162
Carlos Montes <carloss.montes@gmail.com>
2025-03-26 22:24:53 -0600

    Servidor en linea!

---
+++ SpringBoot/springboot-applications/src/main/java/com/cmontes/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -0,0 +1,14 @@
+package com.cmontes.spring.app1.springboot_applications.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EjemploController {
+ 
+    @GetMapping("/detalles_info")
+    public String info() {
+        return "detalles_info";
+    }
+}
